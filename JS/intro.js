0aba8c116a7fa17730232bc9dcdadbd5deb052f3
ijassm <ijass.ocean@gmail.com>
2024-11-19 16:40:12 +0530

    completed variables & datatypes for js

---
+++ JS/intro.js
@@ -0,0 +1,52 @@
+// console.log("Hello");
+// document.write("<h1>Hello</h1>");
+
+// Datatypes
+
+/*
+console.log("Hello");
+console.log('Hello');
+console.log(`Hello`);
+console.log(typeof `Hello`);
+*/
+
+// console.log(45);
+// console.log(typeof 45);
+
+// console.log(45.5);
+// console.log(typeof 45.5);
+
+// console.log(true);
+// console.log(typeof true);
+// console.log(false);
+// console.log(typeof false);
+
+// console.log(undefined);
+// console.log(typeof undefined);
+
+// console.log(null);
+// console.log(typeof null);
+
+// console.log(45n);
+// console.log(typeof 45n);
+
+// console.log(Symbol("Hello") === Symbol("Hello"));
+// console.log("Hello" === "Hello");
+// console.log(typeof Symbol("Hello"));
+
+// variables
+
+// var a;
+// var b = 20;
+
+// a = 10;
+
+// console.log(a, b);
+
+var name = "Sanjai";
+var age = 20;
+var voteEligibility = true;
+
+console.log(`Name: ${name}`);
+console.log(`age: ${age}`);
+console.log(`voteEligibility: ${voteEligibility}`);
